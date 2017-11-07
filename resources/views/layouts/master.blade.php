71f3eaa4f974d7e9dee8e4bdb99985e45b27399d
Manson Xasthur <mansonx13@gmail.com>
2017-11-07 13:40:36 +0200

    add favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="stylesheet" href="{{ asset('css/cosmo.css') }}" media="screen">
-    <link rel="stylesheet" href="{{ asset ('css/custom.min.css') }}">
-    <link rel="stylesheet" href="{{ asset ('css/style.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('css/custom.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('favicon.png') }}">
 
     <script>
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,10 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <title>El Moled CMS</title>
+
+    <link rel="icon" href="{{ asset('favicon.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+
 
     <title>Signin Template for Bootstrap</title>
 
